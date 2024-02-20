b8cb14d2d803c013bd078ccd23513e8524fb1031
Akshat <akshatjain481@gmail.com>
2024-02-20 22:36:06 +0530

    added cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
-
+import { LayoutGridDemo } from "@/components/ui/LayoutGridDemo";
 export default function Home() {
   return (
     <main>
       <section>
         <Button>Test</Button>
+        <LayoutGridDemo></LayoutGridDemo>
       </section>
     </main>
   );
